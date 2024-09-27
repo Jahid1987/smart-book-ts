9a54b8a613e251e1d1662b2d1165589ff6718649
Jahid Anwer <anwerjahid3@gmail.com>
2024-09-27 20:50:54 +0600

    edit: books, bookcard

--- src/components/front-end/home/BookCard.tsx
+++ src/components/front-end/home/BookCard.tsx
@@ -6,17 +6,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-const BookCard = () => {
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+const BookCard = ({
+  name,
+  details,
+  btn,
+  children,
+}: {
+  name: string;
+  details: string;
+  btn: { name: string; link: string };
+  children: React.ReactNode;
+}) => {
   return (
     <div>
       <Card className="transform hover:border-primary ">
-        <CardHeader>
-          <CardTitle>This is header</CardTitle>
+        <CardHeader className=" hover:text-primary">
+          <CardTitle className="flex gap-2 items-center">
+            {children}
+            {name}
+          </CardTitle>
         </CardHeader>
         <CardContent>
-          <CardDescription>This is content</CardDescription>
+          <CardDescription>{details}</CardDescription>
         </CardContent>
-        <CardFooter>This is footer</CardFooter>
+        <CardFooter>
+          <Link href={btn.link}>
+            <Button className="cursor-pointer">{btn.name}</Button>
+          </Link>
+        </CardFooter>
       </Card>
     </div>
   );
--- src/components/front-end/home/Books.tsx
+++ src/components/front-end/home/Books.tsx
@@ -1,12 +1,49 @@
 import SectionTitle from "@/components/shared/SectionTitle";
 import BookCard from "./BookCard";
-
+import { FaCalculator } from "react-icons/fa";
+import { TbAbc } from "react-icons/tb";
+import { LuBinary } from "react-icons/lu";
 const Books = () => {
   return (
     <section>
       <SectionTitle title="Let's Explore All Books" />
       <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <BookCard />
+        <BookCard
+          name="ICT"
+          details="This is ICT Paper - HSC"
+          btn={{ name: "Read Now", link: "/books/accounting1" }}
+        >
+          <LuBinary className="text-3xl" />
+        </BookCard>
+        <BookCard
+          name="English 1st"
+          details="This is English 1st Paper - HSC"
+          btn={{ name: "Read Now", link: "/books/accounting1" }}
+        >
+          <TbAbc className="text-3xl" />
+        </BookCard>
+        <BookCard
+          name="English 2nd"
+          details="This is English 2nd Paper - HSC"
+          btn={{ name: "Read Now", link: "/books/accounting1" }}
+        >
+          <TbAbc className="text-3xl" />
+        </BookCard>
+
+        <BookCard
+          name="Accounting 1st"
+          details="This is Accounting 1st - HSC"
+          btn={{ name: "Read Now", link: "/books/accounting1" }}
+        >
+          <FaCalculator className="text-3xl" />
+        </BookCard>
+        <BookCard
+          name="Accounting 2nd"
+          details="This is Accounting 2nd - HSC"
+          btn={{ name: "Read Now", link: "/books/accounting1" }}
+        >
+          <FaCalculator className="text-3xl" />
+        </BookCard>
       </div>
     </section>
   );
